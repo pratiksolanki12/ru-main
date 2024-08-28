cafdead9fbf1709c1cecfa90186bdc9f851df3bd
pratik solanki <solankipratik605@gmail.com>
2024-08-28 16:26:00 +0530

    Add files via upload

---
+++ ru.js
@@ -0,0 +1,36 @@
+box1.addEventListener("click", () => {
+    div1.style.display = "block";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+})
+box2.addEventListener("click", () => {
+    div1.style.display = "none";
+    div2.style.display = "block";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+})
+box3.addEventListener("click", () => {
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "none";
+    div5.style.display = "none";
+})
+box4.addEventListener("click", () => {
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "block";
+    div5.style.display = "none";
+})
+box5.addEventListener("click", () => {
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "block";
+})
+
